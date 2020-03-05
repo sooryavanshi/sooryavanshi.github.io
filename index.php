97c51e24707569e2da3da04aed60ba12e8536a1a
sooryavanshi <61816874+sooryavanshi@users.noreply.github.com>
2020-03-05 21:41:55 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,95 @@
+
+<html>
+	<head>
+		<link rel="stylesheet" href="./css/main.css" >
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Free Privacy Policy Generator for Website</title>
+<script data-ad-client="ca-pub-4801483727997244" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>	</head>
+<script>
+	function copyScript(element) {
+  var copyText = document.getElementById(element);
+  copyText.select();
+  document.execCommand("copy");
+}
+</script>
+	<body>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144189084-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-144189084-1');
+</script>
+		<div class="container">
+			<h1><a href="http://webbeast.in">WebBeast</a></h1>
+<p>We Deliver Value</p>
+<nav class="navbar navbar-light navbar-light" style="background-color: #e3f2fd;">
+<a class="navbar-brand" href="http://webbeast.in/buy-cheapest-COM-domain-name-godaddy.php">BUY CHEAPEST .COM Domains HERE</a>
+<a class="navbar-brand" href="https://gplbeast.com">BUY CHEAPEST GPL Plugins + Themes</a>
+<!--<a class="navbar-brand" href="/best-deals.php">Hosting Offers & Promos</a>-->
+<a class="navbar-brand" href="http://webbeast.in/Adsense-Approval-Guide-For-Beginners.php">Adsense Approval Guide</a>
+<a class="navbar-brand" href="http://webbeast.in/Start-Lyrics-Website-From-Scratch.php">Start Lyrics Website From Scratch</a>
+</nav>			<div class="col-xl-8 col-lg-9 col-sm-12">
+								<div class="panel panel-success">
+<div class="panel-heading">Free Privacy Policy Generator for Website</div>
+					<div class="panel-body">
+						<table>
+							<form method='post'>
+								<tr><th>Site name</th><td><input type='text' name='blog_name' placeholder='Enter Blog Name' class='form-control' required/></td></tr>
+								<tr><th>Site URL</th><td><input type='text' name='url' placeholder='Enter URL' class='form-control' required/></td></tr>
+								<tr><th>About Site</th><td><input type='text' name='about' placeholder='Enter Something About Your Site' class='form-control' required/></td></tr>
+								<tr><th>Name</th><td><input type='text' name='name' placeholder='Enter Full Name' class='form-control' required/></td></tr>
+								<tr><th>Email ID</th><td><input type='text' name='email' placeholder='Enter Email ID' class='form-control' required/></td></tr>
+								<tr><td></td><td><input type='submit' name='submit' value='Submit' class='btn btn-success' /></td></tr>
+							</form>
+						</table>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-4 col-lg-3 col-sm-12">
+<div class="panel panel-primary">
+    <div class="panel-heading">BEST DEALS</div>
+      <ul class="list-group">
+        <li class='list-group-item'><a href='http://webbeast.in/promo/a2hosting'><img src='https://www.google.com/s2/favicons?domain=a2hosting.com' /> a2Hosting.com</a></li>
+        <li class='list-group-item'><a href='http://webbeast.in/promo/namecheap'><img src='https://www.google.com/s2/favicons?domain=namecheap.com' /> NameCheap.com</a></li>
+        <li class='list-group-item'><a href='http://webbeast.in/promo/hostgator'><img src='https://www.google.com/s2/favicons?domain=hostgator.com' /> HostGator.com</a></li>
+      </ul>
+  </div>
+	
+<div class="panel panel-primary">
+    <div class="panel-heading">Popular BEAST TOOLS</div>
+			<ul class="list-group">
+				<li class='list-group-item'><a href='http://webbeast.in/free-online-wishing-script-maker.php'>Wishing Script Maker</a></li>
+				<li class='list-group-item'><a href="http://webbeast.in/youtube-player-embed-code-generator.php">YouTube Embed Code Generator</a></li>
+				<li class='list-group-item'><a href="http://webbeast.in/video-player-embed-code-generator.php">Video Embed Code Generator</a></li>
+				<li class='list-group-item'><a href="http://webbeast.in/beta.php">Create Lyrics Post</a></li>
+				<li class='list-group-item'><a href="http://webbeast.in/policy-creator.php">Policy Pages Creator</a></li>
+				<li class='list-group-item'><a href="http://webbeast.in/get-premium-wordpress-theme.php">Premium Wordpress Theme</a></li>
+			</ul>
+	</div>
+
+   <div class="panel panel-primary">
+    <div class="panel-heading">Follow Us</div>
+			<ul class="list-group">
+				<li class='list-group-item'><img src='https://www.google.com/s2/favicons?domain=youtube.com' /> <a href='https://www.youtube.com/channel/UCdNmPo8nwuQ6M0ypcGL9ZnA/'>YouTube</a></li>
+				<li class='list-group-item'><img src='https://www.google.com/s2/favicons?domain=instagram.com' /> <a href='https://webbeast.in/instagram/'>InstaGram</a></li>
+				<li class='list-group-item'><img src='https://www.google.com/s2/favicons?domain=web.telegram.org' /> <a href='https://t.me/webbeastyt'>TeleGram</a></li>
+			</ul>
+	</div>
+</div>		</div>
+		<p style='text-align: center;'>
+<a href="http://webbeast.in/about-us.php">About Us</a>
+<a href="http://webbeast.in/contact-us.php">Contact Us</a>
+<a href="http://webbeast.in/privacy-policy.php">Privacy Policy</a>
+<a href="http://webbeast.in/terms-and-condition.php">Terms & Conditions</a>
+<a href="http://webbeast.in/return-policy.php">Return Policy</a>
+<a href="http://webbeast.in/copyright-free-image-generator-tool.php">Target 50K Subscribers(29th Feb Se Pehle)</a>
+</p>
+<br/>
+<footer class="text-center">&copy; <a href="#">WebBeast</a> All Rights Reserved.</footer>	</body>
+</html>
